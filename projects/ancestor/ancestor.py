67c1afd7f52ed20b00a4a2411ce18c2808476014
Anthony Sutton <anthony.sutton95@gmail.com>
2020-04-15 10:19:26 -0700

    Compelted ancestor project

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Graph:
     def __init__(self):
         self.vertices = {}
@@ -20,6 +23,48 @@ class Graph:
             raise Exception("Vertex ID was not found")
 
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+class Queue():
+    def __init__(self):
+        self.queue = []
 
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+def earliest_ancestor(ancestors, starting_node):
+    graph = Graph()
+
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+
+        graph.add_edge(pair[1], pair[0])
+
+    queue = Queue()
+    queue.enqueue([starting_node])
+    max_path_length = 1
+    earliest_ancestor = -1
+
+    while queue.size() > 0:
+        path = queue.dequeue()
+        v = path[-1]
+
+        if (len(path) >= max_path_length and v < earliest_ancestor) or (len(path) > max_path_length):
+            earliest_ancestor = v
+            max_path_length = len(path)
+
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            queue.enqueue(path_copy)
+
+    return earliest_ancestor
